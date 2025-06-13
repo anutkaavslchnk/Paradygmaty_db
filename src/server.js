4896d004f444a2be0392de65255648b71819a508
Anna Vasylchenko <anutochkaaaaa14@gmail.com>
2025-06-14 01:28:48 +0200

    edit todos

--- src/server.js
+++ src/server.js
@@ -11,7 +11,10 @@ export const startServer=()=>{
 const app=express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}));
 app.use(
     pino({
       transport: {
